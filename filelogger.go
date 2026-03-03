24cffbd2ad3f6a4ac1d160e3e5fcde53fd41d8f8
Ayush Singhal <ayush.singhal@grofers.com>
2026-03-04 01:06:55 +0530

    Implement FileLogger for improved file logging and concurrency management

---
+++ filelogger.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"time"
+)
+
+type FileLogger struct {
+	file   *os.File
+	writer *bufio.Writer
+	ch     chan []byte
+	done   chan struct{}
+}
+
+func NewFileLogger(filename string) *FileLogger {
+	file, _ := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	writer := bufio.NewWriter(file)
+
+	ch := make(chan []byte, channelSize)
+
+	fileLogger := &FileLogger{
+		file:   file,
+		writer: writer,
+		ch:     ch,
+		done:   make(chan struct{}),
+	}
+
+	go fileLogger.run()
+
+	return fileLogger
+}
+
+func (f *FileLogger) run() {
+	for msg := range f.ch {
+		f.writer.Write(msg)
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	// channel closed → flush remaining data
+	f.writer.Flush()
+	f.file.Close()
+
+	close(f.done)
+}
+
+func (f *FileLogger) Log(b []byte) {
+	f.ch <- b
+}
+
+func (f *FileLogger) Close() {
+	close(f.ch) // signal no more logs
+	<-f.done
+}
--- main.go
+++ main.go
@@ -7,36 +7,27 @@ import (
 	"time"
 )
 
-const channelSize = 2
+const channelSize = 100
 
 func main() {
 	filename := "my-file.txt"
-	file, _ := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
-	buffer := bufio.NewWriter(file)
-	defer func() {
-		buffer.Flush()
-		file.Close()
-	}()
+	fileLogger := NewFileLogger(filename)
 
-	ch := make(chan []byte, channelSize)
-
-	go func() {
-		for i := 0; i < 300; i++ {
-			data := fmt.Sprintf("\nDevansh Singhal, %d", i)
-			byteData := []byte(data)
-			fmt.Println("time: ", time.Now())
-			ch <- byteData
-		}
-		close(ch)
-	}()
-
-	for data := range ch {
-		fmt.Println("consumed ")
-		dataSize := len(data)
-		buffer.Write(data)
-		bufferSize := buffer.Buffered()
-		fmt.Println(fmt.Sprintf("bufferSize: %d bytes, written bytes: %d", bufferSize, dataSize))
-		time.Sleep(1 * time.Second)
+	for i := 0; i < 300; i++ {
+		data := fmt.Sprintf("\nDevansh Singhal, %d", i)
+		byteData := []byte(data)
+		fmt.Println(fmt.Sprintf("producer 1: time: %s, i: %d", time.Now(), i))
+		fileLogger.Log(byteData)
 	}
 
+	time.Sleep(5 * time.Second)
+
+	for i := 0; i < 300; i++ {
+		data := fmt.Sprintf("\nDevansh Singhal, %d", i)
+		byteData := []byte(data)
+		fmt.Println(fmt.Sprintf("producer 2: time: %s, i: %d", time.Now(), i))
+		fileLogger.Log(byteData)
+	}
+
+	fileLogger.Close()
 }
