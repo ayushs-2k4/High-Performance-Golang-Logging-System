6373e2304dc2a8c49e37e56556b5e424641c753c
Ayush Singhal <ayush.singhal@grofers.com>
2026-03-03 22:17:08 +0530

    Update file creation logic.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 }
 
 func createFileIfNotExists(filename string) (*os.File, error) {
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 	if file == nil || err != nil {
 		return nil, err
 	}
